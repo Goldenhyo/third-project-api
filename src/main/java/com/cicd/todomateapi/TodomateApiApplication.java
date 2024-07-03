9a3465ccfa5e215bb4e12d5058a6de548542c3e9
Goldenhyo <jintop01@naver.com>
2024-07-03 15:40:09 +0900

    first commit

---
+++ src/main/java/com/cicd/todomateapi/TodomateApiApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.todomateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomateApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodomateApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cicd/todomateapi/TodomateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.todomateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodomateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
